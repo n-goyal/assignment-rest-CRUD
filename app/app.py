028c8c5dc432046ea6b5d41f9194d654c1b78a39
Nitin Goyal <goyalnitin634@gmail.com>
2020-07-25 20:05:54 +0530

    adding UPDATE, DELETE, PUT requests

--- app/app.py
+++ app/app.py
@@ -16,18 +16,42 @@ def index():
     # return app
 
 # search university info
-@app.route('/search')
-def getUniversity():
-    search_key = request.args['name']
+@app.route('/search/<search_key>/get-details', methods=['GET'])
+def get_university(search_key):
+    print(search_key)
     matches = uni_details(search_key)
     return jsonify({
         "matches": matches
     })
 
 # delete university information
-# @app.route('/universities/<uni_name>', methods=['DELETE']):
-# def delete_university 
+@app.route('/universities/<uni_name>/delete-records', methods=['DELETE'])
+def delete_university(uni_name):
+    print(uni_name)
+    result = del_details(uni_name)
+    print(result)
+    return result
 
+# insert university details
+@app.route('/universities/create', methods=['POST'])
+def insert_record():
+    data = request.get_json()
+    result = create_record(data)
+    # return incoming
+    return jsonify({
+        "data": data,
+        "status_message": result
+    })
+
+# update university details
+@app.route('/universities/<uni_name>/update', methods=['PUT'])
+def update_details(uni_name):
+    data = request.get_json()
+    result = update_record(uni_name, data)
+    # return jsonify({
+    #     "result": result
+    # })
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/dataHelper/data_Helper.py
+++ app/dataHelper/data_Helper.py
@@ -1,6 +1,7 @@
 import json
 
 '''
+template JSON object:
 {
     "alpha_two_code": "US",
     "country": "United States",
@@ -9,66 +10,72 @@ import json
     "web_page": "http://www.acu.edu/"
 }
 '''
-
-# class University:
-#     def __init__(self, alphaCode, country, domain, name, web):
-#         self.alphaCode = alphaCode
-#         self.country = country
-#         self.domain = domain
-#         self.name = name 
-#         self.web = web
-
-
+# read JSON
 
 jsonFile = open('universities.json')
 jsonData = json.load(jsonFile)
 jsonFile.close()
 
-'''
-data = 
-[
-    {
-        id: 1,
-        uni:{
-
-        }
-    }
-]
-'''
-
-# def add_id():
-#     print(len(jsonData))
-#     # assign Id to each object with name
-#     for i in range(len(jsonData)):
-#         data[jsonData[i]['name']] = jsonData[i]
-#     search = ''
-#     return data
-
-
 # index
 def get_all():
     return jsonData
 
-# Search - GET
+# GET - Search
 def uni_details(search_key):
-    # print(data.keys())
     matches = []
-    for i in range(len(jsonData)):
-        if search_key in jsonData[i]['name']:
-            matches.append(jsonData[i])
+    for obj in jsonData:
+        if search_key in obj['name']:
+            matches.append(obj)
     # matches = [value for key, value in data.items() if name in key]
     return matches
 
-# delete - Record
-def del_details(name):
-    # remove element from data and jsonData
-    # save new json file
-    return ''
+# DELETE - Record
+def del_details(uni_name, data=jsonData):
+    x = len(data)
+    data = [obj for obj in data if obj['name'] != uni_name]
+    y = len(data)
+    if x != y:
+        # data = json.dumps(cleanData)
+        with open("universities.json","w") as jsonFile:
+            json.dump(data, jsonFile)
+        return 'Success'
+    else:
+        return 'No record was found'
 
+# CREATE - Record
+def create_record(record):
+    jsonData.append(record)
+    with open("universities.json", "w") as jsonFile:
+        json.dump(jsonData, jsonFile)
+    return 'Inserted Successfully'
 
+# UPDATE - Record
+def update_record(uni_name, data):
+        # update any matching articles
+    for obj in jsonData:
+        if obj['name'] == uni_name:
+            obj['alpha_two_code'] = data['alpha_two_code']
+            obj['country'] = data['country']
+            obj['domain'] = data['domain']
+            obj['name'] = data['name']
+            obj['web_page'] = data['web_page']
+            break
+
+    # rewrite the whole JSON file with updated dictionary
+    with open("universities.json", "w") as jsonFile:
+        json.dump(jsonData, jsonFile)
+
+    return 'Updated Successfully'
+
+# testing purpose
 if __name__=='__main__':
-    # print(getJSON())
-    # print(len(jsonData))
-    print(json)
-    # print(len(getUniversity('Ad')))
-    # print(addId())
+    # get_all()
+    print(len(jsonData))
+    # print(del_details('Ho Chi Minh City University of Natural Sciences'))
+    # print(update_record("Ho Chi Minh City University of Natural Sciences", {
+    #     "country": "Viet Nam", 
+    #     "alpha_two_code": "VN", 
+    #     "web_page": "http://www.CHANIGN.vnn.vn/", 
+    #     "domain": "huflit.vnn.vn", 
+    #     "name": "Ho Chi Minh City University of Natural Sciences"}))
+    print(len(jsonData))
