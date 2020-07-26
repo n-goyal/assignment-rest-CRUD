35d4651719b775eede1f029c0f14885dd5c51f1f
Nitin Goyal <goyalnitin634@gmail.com>
2020-07-26 21:25:32 +0530

    removed error

--- app/static/script.js
+++ app/static/script.js
@@ -110,9 +110,6 @@ document.getElementById('btn-search').addEventListener('click', function() {
   }
 })
 
-// update button
-document.getElementById()
-
 
 function loadIndex(page) {
   fetch(url='http://127.0.0.1:5000/universities/'+page, {
